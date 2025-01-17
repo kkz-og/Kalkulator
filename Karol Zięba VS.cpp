d0d15604cea5dc2122661e58f5a094e9e6bc441c
kkz-og <xkarolx1@o2.pl>
2025-01-17 19:37:42 +0100

    Add files via upload

---
+++ Karol Zięba VS.cpp
@@ -0,0 +1,39 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+    char operation;
+    double num1, num2;
+
+    cout << "Prosty kalkulator" << endl;
+    cout << "Podaj operator (+, -, *, /): ";
+    cin >> operation;
+
+    cout << "Podaj dwie liczby: " << endl;
+    cin >> num1 >> num2;
+
+    switch (operation) {
+    case '+':
+        cout << "Wynik: " << num1 + num2 << endl;
+        break;
+    case '-':
+        cout << "Wynik: " << num1 - num2 << endl;
+        break;
+    case '*':
+        cout << "Wynik: " << num1 * num2 << endl;
+        break;
+    case '/':
+        if (num2 != 0) {
+            cout << "Wynik: " << num1 / num2 << endl;
+        }
+        else {
+            cout << "Blad: Dzielenie przez zero!" << endl;
+        }
+        break;
+    default:
+        cout << "Nieprawidlowy operator!" << endl;
+    }
+
+    return 0;
+}
